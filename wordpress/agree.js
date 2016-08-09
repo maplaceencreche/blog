3de965d9b6bc6cd88285d04761d3eea6a0682ac2
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2016-08-09 11:35:57 +0200

    Remove console logs

--- wordpress/agree.js
+++ wordpress/agree.js
@@ -2,15 +2,12 @@
 
 jQuery(document).ready(function ($) {
   var submitURL = $('#btn-inscript').attr('href');
-  console.log(submitURL);
 
   $('#agreed').change(function () {
     if ($(this).is(':checked')) {
       $('#btn-inscript').attr('href', submitURL + '&mpec_agree=true');
-      console.log('check');
     } else {
       $('#btn-inscript').attr('href', submitURL);
-      console.log('uncheck');
     }
   });
 });
